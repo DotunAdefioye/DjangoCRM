3a9446b22c093444c47ad5aa71c4699811474543
DotunAdefioye <dotunadefioye1@gmail.com>
2024-06-29 00:00:06 -0700

    new

--- website/forms.py
+++ website/forms.py
@@ -5,17 +5,14 @@ from django.core.exceptions import ValidationError
 import re
 
 class SignUpForm(UserCreationForm):
-    user_name = forms.CharField(label="Username", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'User Name'}))
     email = forms.EmailField(label="Email", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email Address'}))
     first_name = forms.CharField(label="First Name", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}))
     last_name = forms.CharField(label="Last Name", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}))
-    password1 = forms.CharField(label="Password", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
-    password2 = forms.CharField(label="Password", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Type same password'}))
     phone_number = forms.CharField(label="Phone Number", max_length=20, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone Number'}))
     date_of_birth = forms.DateField(label='Date of birth', required=False, widget=forms.DateInput(attrs={'class': 'form-control', 'placeholder': 'YYYY-MM-DD'}))
-    gender = forms.CharField(max_length=1)
-    marital_status = forms.CharField(max_length=1)
-    address = forms.CharField(max_length=255, required=False, widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Address'}))
+    gender = forms.CharField(label="Gender", max_length=6, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'gender'}))
+    marital_status = forms.CharField(label="Marital Status", max_length=20, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Marital Status'}))
+    address = forms.CharField(max_length=100, required=False, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Address'}))
 
     class Meta:
         model = User
@@ -32,15 +29,15 @@ class SignUpForm(UserCreationForm):
 
         self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields['username'].widget.attrs['placeholder'] = 'User Name'
-        self.fields['username'].label = ''
+        self.fields['username'].label = 'Username'
         self.fields['username'].help_text = '<span class="form-text text-muted"><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
 
         self.fields['password1'].widget.attrs['class'] = 'form-control'
         self.fields['password1'].widget.attrs['placeholder'] = 'Password'
-        self.fields['password1'].label = ''
+        self.fields['password1'].label = 'Password'
         self.fields['password1'].help_text = '<ul class="form-text text-muted small"><li>Your password can\'t be too similar to your other personal information.</li><li>Your password must contain at least 8 characters.</li><li>Your password can\'t be a commonly used password.</li><li>Your password can\'t be entirely numeric.</li></ul>'
 
         self.fields['password2'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
-        self.fields['password2'].label = ''
+        self.fields['password2'].label = 'Password'
         self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
--- website/models.py
+++ website/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
+class Record(model.Model):
+    created_at = models.DateTimeField(auto_created=True)
+    
+
 # Create your models here.
