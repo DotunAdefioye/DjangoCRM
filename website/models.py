5bb8928642e870fc3f0ffac09b2ad35f89f94b3a
DotunAdefioye <dotunadefioye1@gmail.com>
2024-06-29 00:29:03 -0700

    update

--- website/models.py
+++ website/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 
-
-class Record(model.Model):
-    created_at = models.DateTimeField(auto_created=True)
-    
-
 # Create your models here.
+class Record(models.Model):
+    created_at = models.DateTimeField(auto_created=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zipcode = models.CharField(max_length=50)
+
+    def __str__(self):
+        return(f'{self.first_name} {self.last_name}')
+
+
+
+
